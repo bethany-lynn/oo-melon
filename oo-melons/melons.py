fc4e7181f569be3e6404adc721801dc1ba894be0
bethany <bethanylynnpro@gmail.com>
2022-12-22 12:47:35 -0800

    melon progress

--- oo-melons/melons.py
+++ oo-melons/melons.py
@@ -1,57 +1,78 @@
 """Classes for melon orders."""
+# create super class AbstractMelonOrder 
+# add shared methods
 
-
-class DomesticMelonOrder:
-    """A melon order within the USA."""
-
+class AbstractMelonOrder():
+    
     def __init__(self, species, qty):
-        """Initialize melon order attributes."""
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
 
     def get_total(self):
-        """Calculate price, including tax."""
-
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
 
         return total
 
     def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
 
         self.shipped = True
 
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+        super().__init__(species, qty) # had to have these two arguments still to match parent class 
+         
 
-class InternationalMelonOrder:
+        # self.species = species
+        # self.qty = qty
+        # self.shipped = False
+        self.order_type = "domestic"
+        self.tax = 0.08
+
+    # def get_total(self):
+    #     """Calculate price, including tax."""
+
+    #     base_price = 5
+    #     total = (1 + self.tax) * self.qty * base_price
+
+    #     return total
+
+    # def mark_shipped(self):
+    #     """Record the fact than an order has been shipped."""
+
+    #     self.shipped = True
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
+        super().__init__(species, qty)
+        # self.species = species
+        # self.qty = qty
+        # self.shipped = False
         self.country_code = country_code
-        self.shipped = False
         self.order_type = "international"
         self.tax = 0.17
 
-    def get_total(self):
-        """Calculate price, including tax."""
+    # def get_total(self):
+    #     """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+    #     base_price = 5
+    #     total = (1 + self.tax) * self.qty * base_price
 
-        return total
+    #     return total
 
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
+    # def mark_shipped(self):
+    #     """Record the fact than an order has been shipped."""
 
-        self.shipped = True
+    #     self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
